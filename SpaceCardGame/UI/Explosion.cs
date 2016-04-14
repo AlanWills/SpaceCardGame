b968c7b281ca198a04f1a726a60eb3fabf2676a8
AlanWills <alawills@googlemail.com>
2016-04-15 00:34:23 +0100

    Reimplimented the Animation as a module and removed the AnimatedUIObject.

--- SpaceCardGame/UI/Explosion.cs
+++ SpaceCardGame/UI/Explosion.cs
@@ -7,7 +7,7 @@ namespace SpaceCardGame
     /// A class which represents an explosion in our game.
     /// Nothing more than an animated image, but as it will be useful it makes sense to add it to as a separate class.
     /// </summary>
-    public class Explosion : AnimatedUIObject
+    public class Explosion : AnimatedGameObject
     {
         public Explosion(Vector2 localPosition) :
             this(Vector2.Zero, localPosition)
